5a00fd90e0497a5f3ea492bd56f0d284fbec8b9e
HoverFo <hoverfox23@gmail.com>
2023-01-09 08:53:14 +0100

    template fonctionnement

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -15,7 +15,7 @@ class UserController extends AbstractController
         $userNotifications = ['...', '...'];
 
         // the template path is the relative file path from `templates/`
-        return $this->render('user/notifications.html.twig', [
+        return $this->render('notifications.html.twig', [
             // this array defines the variables passed to the template,
             // where the key is the variable name and the value is the variable value
             // (Twig recommends using snake_case variable names: 'foo_bar' instead of 'fooBar')
